fb430e54720bee5628f5681cd43472e7f181d889
zapaz.eth <alain@kredeum.com>
2025-01-10 16:01:20 +0100

    test page pin

--- src/routes/tests/+page.svelte
+++ src/routes/tests/+page.svelte
@@ -6,6 +6,7 @@
 {/snippet}
 
 <ul class="list-disc space-y-2 pl-5">
+  {@render pageTest("pin")}
   {@render pageTest("counter")}
   {@render pageTest("connect")}
   <!-- {@render pageTest("burner")} -->
---
+++ src/routes/tests/pin/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import FileUpload from "$lib/components/FileUpload.svelte";
+  import type { FileItem } from "$lib/types";
+
+  const handleFilesSelected = (files: FileItem[]) => {
+    console.log("Files selected:", files);
+  };
+</script>
+
+<div class="flex h-32 flex-col items-center justify-center">
+  <FileUpload onFilesSelected={handleFilesSelected} />
+</div>
