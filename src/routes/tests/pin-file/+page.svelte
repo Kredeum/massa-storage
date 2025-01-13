ad9245967a6f22a49b39252baf41f0cbae8c58d8
zapaz.eth <alain@kredeum.com>
2025-01-13 11:26:20 +0100

    split pin text and file

--- src/routes/tests/pin-file/+page.svelte
+++ src/routes/tests/pin-file/+page.svelte
@@ -3,36 +3,20 @@
   import { CID } from "multiformats/cid";
 
   import { createHelia, type Helia } from "helia";
-  import { strings, type Strings } from "@helia/strings";
   import { unixfs, type UnixFS } from "@helia/unixfs";
   import FileUpload5 from "$lib/components/FileUpload5.svelte";
-  import type { FileItem } from "$lib/types";
-  import { LucideGalleryVerticalEnd } from "lucide-svelte";
 
   let helia: Helia;
-  let s: Strings;
   let fs: UnixFS;
 
-  let dataInput = $state<string>("");
   let cidInput = $state<string>("");
   let files = $state<FileList>();
   let fileInput = $derived<File | undefined>(files?.[0]);
-  let retrievedData = $state<string>("");
+  let fileRetreived = $state<string>("");
 
   onMount(async () => {
     helia = await createHelia();
-    s = strings(helia);
     fs = unixfs(helia);
-  });
-
-  const dataHandle = async (data?: string) => {
-    if (!data?.trim()) return;
-
-    const cid = await s.add(data);
-    cidInput = cid.toString();
-  };
-  $effect(() => {
-    dataHandle(dataInput);
   });
 
   const fileHandle = async (fileData?: File) => {
@@ -55,19 +39,8 @@
     fileHandle(fileInput);
   });
 
-  const retrieveSubmit = async () => {
-    if (!cidInput.trim()) return;
 
-    try {
-      const parsedCid = CID.parse(cidInput);
-      retrievedData = await s.get(parsedCid);
-    } catch (error) {
-      console.error("Error retrieving data:", error);
-      retrievedData = "Error: Could not retrieve data";
-    }
-  };
-
-  const fileDownload = async () => {
+  const fileRetrieve = async () => {
     if (!cidInput.trim()) return;
 
     try {
@@ -79,32 +52,27 @@
         content += decoder.decode(chunk, { stream: true });
       }
 
-      retrievedData = content;
+      fileRetreived = content;
     } catch (error) {
       console.error("Error retrieving file:", error);
-      retrievedData = "Error: Could not retrieve file";
+      fileRetreived = "Error: Could not retrieve file";
     }
   };
 </script>
 
 <div class="flex flex-col items-center justify-center space-y-8 p-4">
   <div class="w-full max-w-3xl space-y-4">
-    <div class="flex gap-2">
-      <input type="text" bind:value={dataInput} placeholder="Enter data to store" class="input" />
-    </div>
-
     <FileUpload5 bind:files />
 
-    <form onsubmit={retrieveSubmit}>
+    <form onsubmit={fileRetrieve}>
       <div class="flex gap-2">
         <input type="text" bind:value={cidInput} placeholder="Enter CID" class="input" />
         <button type="submit" class="button">Retrieve</button>
       </div>
     </form>
-    <!-- <button onclick={fileDownload} class="button">Download File</button> -->
-    {#if retrievedData}
+    {#if fileRetreived}
       <div class="mt-4 rounded bg-gray-100 p-6">
-        <p class="whitespace-pre-wrap text-lg">{retrievedData}</p>
+        <p class="whitespace-pre-wrap text-lg">{fileRetreived}</p>
       </div>
     {/if}
   </div>
