a414c258efcc295272912778a861907c5c7e4a3a
zapaz.eth <alain@kredeum.com>
2025-01-15 15:52:25 +0100

    add Kubo client with addAndPin from browser to ipfs desktop

--- src/routes/tests/+page.svelte
+++ src/routes/tests/+page.svelte
@@ -5,10 +5,10 @@
   <li><a class="text-blue-500 hover:underline" href="/tests/{pageName}">{pageName}</a></li>
 {/snippet}
 
-{#await import.meta.glob("./*/+page.svelte") then modules}
+{#await import.meta.glob("./**/+page.svelte") then modules}
   <ul class="list-disc space-y-2 pl-5">
     {#each Object.keys(modules) as path}
-      {@const pageName = path.split("/")[1]}
+      {@const pageName = path.replace(/^\.\/|\/\+page\.svelte$/g, '')}
       {@render pageTest(pageName)}
     {/each}
   </ul>
