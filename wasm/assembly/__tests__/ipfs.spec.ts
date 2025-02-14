29c11740562b4638f3a773e38fe1aefe50d7e4bd
volkanguneri <guneriv@gmail.com>
2025-02-14 13:02:43 +0100

    wip as-pect issue

--- wasm/assembly/__tests__/ipfs.spec.ts
+++ wasm/assembly/__tests__/ipfs.spec.ts
@@ -1,10 +1,5 @@
-import { Context, Address } from '@massalabs/massa-as-sdk';
-import {
-  bytesToString,
-  stringToBytes,
-  boolToByte,
-  Args,
-} from '@massalabs/as-types';
+import { Address, setDeployContext } from '@massalabs/massa-as-sdk';
+import { stringToBytes, boolToByte, Args } from '@massalabs/as-types';
 import { ownership } from '@massalabs/sc-standards';
 import {
   constructor,
@@ -19,105 +14,69 @@ import {
   cidsGet,
 } from '../contracts/ipfs';
 
+const OWNER_ADDRESS = 'AU1JC5Q7c6mV7TcEtj2yA1Cv49JSiFz4xS8dnbGbmKHbVfrmFHvJ';
+const MODERATOR_ADDRESS =
+  'AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiq';
+
 describe('IPFS Smart Contract', () => {
-  describe('constructor', () => {
-    it('should set owner and add caller as moderator', () => {
-      const mockAddress = new Address(
-        'AU1JC5Q7c6mV7TcEtj2yA1Cv49JSiFz4xS8dnbGbmKHbVfrmFHvJ',
-      );
-      Context.caller = () => mockAddress;
-      Context.isDeployingContract = () => true;
-
-      constructor([]);
-
-      expect(ownership.isOwner(mockAddress.serialize())).toBe(boolToByte(true));
-      expect(moderatorHas(mockAddress.serialize())).toBe(boolToByte(true));
-    });
+  beforeAll(() => {
+    setDeployContext(OWNER_ADDRESS);
+    constructor([]);
   });
 
-  // Tests des fonctions de modération
-  describe('Moderator functions', () => {
-    const mockModerator = new Address(
-      'AU1JC5Q7c6mV7TcEtj2yA1Cv49JSiFz4xS8dnbGbmKHbVfrmFHvJ',
+  test('constructor should set owner', () => {
+    const ownerAddr = new Address(OWNER_ADDRESS);
+    expect(ownership.isOwner(ownerAddr.serialize())).toStrictEqual(
+      boolToByte(true),
     );
-
-    it('should add and check moderator', () => {
-      moderatorAdd(mockModerator.serialize());
-      expect(moderatorHas(mockModerator.serialize())).toBe(boolToByte(true));
-    });
-
-    it('should list moderators', () => {
-      const moderators = moderatorsGet(new Args().add('MODERATOR').serialize());
-      const serializedModerator = mockModerator.serialize();
-
-      expect(moderators.length).toBeGreaterThan(0);
-
-      // Compare arrays manually
-      let found = false;
-      for (let i = 0; i < moderators.length; i++) {
-        if (moderators[i] == serializedModerator[i]) {
-          found = true;
-          break;
-        }
-      }
-      expect(found).toBe(true);
-    });
-
-    it('should delete moderator', () => {
-      moderatorDelete(mockModerator.serialize());
-      expect(moderatorHas(mockModerator.serialize())).toBe(boolToByte(false));
-    });
   });
 
-  // Tests des fonctions CID
-  describe('CID functions', () => {
-    const mockCid = stringToBytes('QmTest123');
+  test('constructor should add owner as moderator', () => {
+    const ownerAddr = new Address(OWNER_ADDRESS);
+    expect(moderatorHas(ownerAddr.serialize())).toStrictEqual(boolToByte(true));
+  });
 
-    it('should add and check CID', () => {
-      cidAdd(mockCid);
-      expect(cidHas(mockCid)).toBe(boolToByte(true));
-    });
+  test('should add new moderator', () => {
+    const moderator = new Address(MODERATOR_ADDRESS);
+    moderatorAdd(moderator.serialize());
+    expect(moderatorHas(moderator.serialize())).toStrictEqual(boolToByte(true));
+  });
 
-    // it('should set CID status', () => {
-    //   const mockCid = stringToBytes('QmTest456');
-    //   const mockStatus = stringToBytes('APPROVED');
+  test('should list moderators', () => {
+    const moderators = moderatorsGet(new Args().add('MODERATOR').serialize());
+    expect(moderators.length).toBeGreaterThan(0);
+  });
 
-    //   cidAdd(new Args().add(mockCid).serialize());
-    //   cidSet(new Args().add(mockCid).add(mockStatus).serialize());
+  test('should delete moderator', () => {
+    const moderator = new Address(MODERATOR_ADDRESS);
+    moderatorDelete(moderator.serialize());
+    expect(moderatorHas(moderator.serialize())).toStrictEqual(
+      boolToByte(false),
+    );
+  });
 
-    //   const cids = cidsGet(new Args().add('CID').serialize());
-    //   let found = false;
-    //   for (let i = 0; i < cids.length; i++) {
-    //     if (bytesToString(cids[i]) === bytesToString(mockCid)) {
-    //       found = true;
-    //       expect(bytesToString(cids[i + 1])).toBe(bytesToString(mockStatus));
-    //       break;
-    //     }
-    //   }
-    //   expect(found).toBe(true);
-    // });
+  const testCid = 'QmTest123';
+  const testCidBytes = stringToBytes(testCid);
 
-    it('should get CIDs', () => {
-      cidAdd(new Args().add(mockCid).serialize());
+  test('should add CID', () => {
+    cidAdd(testCidBytes);
+    expect(cidHas(testCidBytes)).toStrictEqual(boolToByte(true));
+  });
 
-      const cids = cidsGet(new Args().add('CID').serialize());
-      const serializedCid = mockCid;
+  test('should list CIDs', () => {
+    const cids = cidsGet(new Args().add('CID').serialize());
+    expect(cids.length).toBeGreaterThan(0);
+  });
 
-      expect(cids.length).toBeGreaterThan(0);
+  test('should delete CID', () => {
+    cidDelete(testCidBytes);
+    expect(cidHas(testCidBytes)).toStrictEqual(boolToByte(false));
+  });
 
-      let found = false;
-      for (let i = 0; i < cids.length; i++) {
-        if (cids[i] == serializedCid[i]) {
-          found = true;
-          break;
-        }
-      }
-      expect(found).toBe(true);
-    });
-
-    it('should delete CID', () => {
-      cidDelete(mockCid);
-      expect(cidHas(mockCid)).toBe(boolToByte(false));
-    });
+  test('should set CID', () => {
+    const newTestCid = 'QmNewTest456';
+    const newTestCidBytes = stringToBytes(newTestCid);
+    cidSet(newTestCidBytes);
+    expect(cidHas(newTestCidBytes)).toStrictEqual(boolToByte(true));
   });
 });
